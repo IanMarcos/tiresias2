c9fa07ef680e19b50a5c4f3b994dbef04faf76ae
natogourmet <natogourmet@hotmail.com>
2023-05-30 19:17:21 -0500

    FEAT: fixtures use

--- __tests__/controller/material.js
+++ __tests__/controller/material.js
@@ -1,54 +1,12 @@
 import { jest } from '@jest/globals';
 import httpMocks from 'node-mocks-http';
+import mockMaterials from '../fixtures/materials.json';
 
 // jest.mock('../../src/services/material.js', () => ({
 //   getAllMaterials: jest.fn(),
 // }));
 
 // const mockMaterialService = jest.spyOn(materialService, 'getAllMaterials');
-
-const mockMaterials = [
-  {
-    id: 1,
-    titulo: 'Libro',
-    edicion: null,
-    isbn: 123,
-    idiomaCodigo: 'ES',
-    anioPublicacion: 2020,
-    anioProduccion: 2021,
-    destinatarios: null,
-    duracion: null,
-    tamanioFichero: 33.3,
-    resumen: null,
-    urlArchivo: '1684597175223',
-    formatoAccesible: 'Daisy 2.02 solo audio',
-    editorial: 'Planeta',
-    productora: 'UdeA',
-    estadoProduccion: 'Disponible',
-    ciudadPublicacion: {
-      nombre: 'Bogota',
-      paisCodigo: 'CO',
-    },
-    ciudadProduccion: {
-      nombre: 'Houston',
-      paisCodigo: 'US',
-    },
-    categorias: [],
-    autores: [
-      {
-        id: 1,
-        apellido: 'RODAS',
-        nombres: 'JUANA',
-      },
-    ],
-    contribuidores: [],
-    narrador: {
-      id: 2,
-      apellido: 'DOE',
-      nombres: 'JOHN',
-    },
-  },
-];
 
 it('should get products list', async () => {
   jest.unstable_mockModule('../../src/services/material.js', () => ({
