3b98f526271e8c66e5f645174f26ef9f1e1a15af
natogourmet <natogourmet@hotmail.com>
2023-05-31 17:31:46 -0500

    FEAT: complete request tests

--- __tests__/controller/request.js
+++ __tests__/controller/request.js
@@ -9,6 +9,7 @@ describe('Request Controller', () => {
     default: {
       getAllRequests: jest.fn(async () => mockRequests),
       createRequest: jest.fn(async () => ({ id: 1 })),
+      getRequestById: jest.fn(async () => mockRequests[0]),
     },
   }));
 
@@ -26,7 +27,7 @@ describe('Request Controller', () => {
     expect(response._getJSONData().results.length).toEqual(6);
   });
 
-  it('should create a new user', async () => {
+  it('should create a new request', async () => {
     // Arrange
     const { createRequest } = await import('../../src/controllers/request.js');
 
@@ -39,4 +40,23 @@ describe('Request Controller', () => {
     expect(response.statusCode).toEqual(201);
     expect(response._getJSONData().results.id).toEqual(1);
   });
+
+  it('should get a request by id', async () => {
+    // Arrange
+    const { getRequestById } = await import('../../src/controllers/request.js');
+
+    // Act
+    const response = httpMocks.createResponse();
+    const request = httpMocks.createRequest({
+      params: {
+        id: 1,
+      },
+    });
+    await getRequestById(request, response);
+
+    // Assert
+    expect(response.statusCode).toEqual(200);
+    expect(response._getJSONData().result.id).toEqual(3);
+  });
+
 });
