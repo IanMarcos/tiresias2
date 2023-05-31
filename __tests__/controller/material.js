8e67465dff699d51fe4302c7a005e5cbd55e4504
natogourmet <natogourmet@hotmail.com>
2023-05-31 13:00:22 -0500

    FEAT: Create Material controller tests

--- __tests__/controller/material.js
+++ __tests__/controller/material.js
@@ -1,6 +1,7 @@
 import { jest } from '@jest/globals';
 import httpMocks from 'node-mocks-http';
 import mockMaterials from '../fixtures/materials.json';
+import materialPostMock from '../fixtures/materialPost.json';
 
 // jest.mock('../../src/services/material.js', () => ({
 //   getAllMaterials: jest.fn(),
@@ -9,26 +10,43 @@ import mockMaterials from '../fixtures/materials.json';
 // const mockMaterialService = jest.spyOn(materialService, 'getAllMaterials');
 
 describe('Material Controller', () => {
-  it('should get all materials', async () => {
-    jest.unstable_mockModule('../../src/services/material.js', () => ({
-      default: {
-        getMaterials: jest.fn(async () => ({ materials: mockMaterials })),
-      },
-    }));
+  // Arrange
+  jest.unstable_mockModule('../../src/services/material.js', () => ({
+    default: {
+      getMaterials: jest.fn(async () => ({ materials: mockMaterials })),
+      createMaterial: jest.fn(async () => ({ id: 1 })),
+    },
+  }));
 
+  it('should get all materials', async () => {
+    // Arrange
     const { getAllMaterials } = await import(
       '../../src/controllers/material.js'
     );
-    // mock
+
+    // Act
     const response = httpMocks.createResponse();
     const request = httpMocks.createRequest();
-    // const mockMaterialsList = jest.fn(async () => ({ materials: mockMaterials }));
-    // mockMaterialService.mockImplementation(mockMaterialsList);
     await getAllMaterials(request, response);
-    // expect(mockMaterialService).toHaveBeenCalledTimes(1);
+
+    // Assert
     expect(response.statusCode).toEqual(200);
-    // expect(response._isEndCalled()).toBeTruthy();
-    // console.log(response);
     expect(response._getJSONData().results.materials.length).toEqual(1);
+  });
+
+  it('should create a new material', async () => {
+    // Arrange
+    const { createMaterial } = await import(
+      '../../src/controllers/material.js'
+    );
+
+    // Act
+    const response = httpMocks.createResponse();
+    const request = httpMocks.createRequest(materialPostMock);
+    await createMaterial(request, response);
+
+    // Assert
+    expect(response.statusCode).toEqual(201);
+    expect(response._getJSONData().results.id).toEqual(1);
   });
 });
